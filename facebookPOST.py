692f21a87aed91347f8f18386ec6b207a0d872ff
Joshua S. Andrews <91918985+Heero1224@users.noreply.github.com>
2026-03-18 10:36:31 -0700

    Update facebookPOST.py
    Made facebookPOST.py callable from an external script and fixed functions to work internally

--- facebookPOST.py
+++ facebookPOST.py
@@ -5,27 +5,25 @@ purpose: to post a message to a facebook feed utilizing facebook-sdk
 """
 import facebook
 
-graph = facebook.GraphAPI("access_token=token")
-
-if len(sys.argv) == 2:
+def img(token, message, image_path):
+    graph = facebook.GraphAPI("access_token="+token)
 	graph.put_photo(image=open(image_path, 'rb'), message)
-else if len(sys.argv) == 3:
+def txt(token, msg):
+    graph = facebook.GraphAPI("access_token="+token)
 	graph.put_object(parent_object='me', connection_name='feed', message)
-else:
-    print('Got to posting without a post?')
-
 
 if __name__ == "__main__":
+    import sys
     '''
     sys.args
         1 = access_token
         2 = message
         3 = image_path
     '''
-    if len(sys.argv) < 2:
-        token= sys.argv[1]
-        message = sys.argv[2] 
-        if len(sys.argv) == 3:
-            image_path = sys.argv[3]
+    length=len(sys.argv)
+    if length==2:
+        txt(sys.argv[1],sys.argv[2])
+    else if length==3:
+        img(sys.argv[1],sys.argv[2],sys.argv[3])
     else
         print("Needs [Token, Message, <image>] to POST to facebook")
