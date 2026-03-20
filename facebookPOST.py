192579db88285c1ec0a8129f28677756e27a957e
Joshua S. Andrews <91918985+Heero1224@users.noreply.github.com>
2026-03-19 18:23:42 -0700

    Update facebookPOST.py
    Changed post txt message to requests api

--- facebookPOST.py
+++ facebookPOST.py
@@ -13,12 +13,33 @@ def img(token, message, image_path):
         print(f"Error posting img to facebook: {e}")
 
 def txt(token, msg):
-    graph = facebook.GraphAPI(access_token=token)
+    import requests
+    
+    page_id=getFacebook_page_id(token)
+    post_url= f'https://graph.facebook.com/{page_id}/feed
+    
+    payload = { 'message': msg,
+                'access_token': token
+    }
+    
     try:
-        graph.put_object(parent_object='me', connection_name='feed', message)
-    except:
-        print(f"Error posting txt to facebook: {e}")
+        response = requests.post(post_url, data=payload)
+        response.raise_for_status()
         
+        post_id = response.json().get('id')
+        print(f"Successfully posted: Post ID: {post_id}")
+
+def getFacebook_page_id(token):
+    ''' 
+    Retrieve facebook ID page associated with provided token
+    '''
+    url=f'https://graph.facebook.com/{token}'
+    response=requests.get(url)
+    data=response.json()
+    
+    page_id = data.get("id")
+    return page_id
+    
 if __name__ == "__main__":
     import sys
     '''
