d21eacbfef3faac13fb881c0f5460cecac7d9585
Joshua S. Andrews <91918985+Heero1224@users.noreply.github.com>
2026-03-19 17:11:21 -0700

    Update facebookPOST.py
    Added try/excepts for posting 
    fixed syntax errors

--- facebookPOST.py
+++ facebookPOST.py
@@ -6,12 +6,19 @@ purpose: to post a message to a facebook feed utilizing facebook-sdk
 import facebook
 
 def img(token, message, image_path):
-    graph = facebook.GraphAPI("access_token="+token)
-	graph.put_photo(image=open(image_path, 'rb'), message)
-def txt(token, msg):
-    graph = facebook.GraphAPI("access_token="+token)
-	graph.put_object(parent_object='me', connection_name='feed', message)
+    graph = facebook.GraphAPI(access_token=token)
+    try:
+        graph.put_photo(image=open(image_path, 'rb'), message)
+    except:
+        print(f"Error posting img to facebook: {e}")
 
+def txt(token, msg):
+    graph = facebook.GraphAPI(access_token=token)
+    try:
+        graph.put_object(parent_object='me', connection_name='feed', message)
+    except:
+        print(f"Error posting txt to facebook: {e}")
+        
 if __name__ == "__main__":
     import sys
     '''
@@ -21,9 +28,9 @@ if __name__ == "__main__":
         3 = image_path
     '''
     length=len(sys.argv)
-    if length==3:
+    if length==2:
         txt(sys.argv[1],sys.argv[2])
-    else if length==4:
+    elif length==3:
         img(sys.argv[1],sys.argv[2],sys.argv[3])
-    else
+    else:
         print("Needs [Token, Message, <image>] to POST to facebook")
