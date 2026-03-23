b5ba55f55ae48626ee3390f40d6386b57c106a5b
Joshua S. Andrews <91918985+Heero1224@users.noreply.github.com>
2026-03-23 11:29:42 -0700

    Update facebookPOST.py
    Reworked posting to utilize facebook SDK and get user's PAGE_ID

--- facebookPOST.py
+++ facebookPOST.py
@@ -6,19 +6,72 @@ purpose: to post a message to a facebook feed utilizing facebook-sdk
 import facebook
 
 def img(token, message, image_path):
-    graph = facebook.GraphAPI(access_token=token)
+    import requests
+    
+    page_id=getFacebook_page_id()
+    
+    post_url= f'https://graph.facebook.com/{page_id}/feed'
+    
+    params = {
+        'access_token': token
+    }
+    data = {
+        'message': msg
+    }
+    files = {
+        'source': open(image_path, 'rb')
+    }
+    
+
     try:
-        graph.put_photo(image=open(image_path, 'rb'), message=message)
-    except:
-        print(f"Error posting img to facebook: {e}")
+        response = requests.post(url=post_url, params=params, data=data, files=files)
+        
+        if response.status_code == 200:
+            post_id = response.json().get('id')
+            print(f"Successfully posted: Post ID: {post_id}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
 
 def txt(token, msg):
-    graph = facebook.GraphAPI(access_token=token)
+    import requests
+    
+    page_id=getFacebook_page_id()
+    
+    post_url= f'https://graph.facebook.com/{page_id}/feed'
+    
+    payload = { 'message': msg,
+                'access_token': token
+    }
+    
     try:
-        graph.put_object(parent_object='me', connection_name='feed', message=msg)
-    except:
-        print(f"Error posting txt to facebook: {e}")
+        response = requests.post(post_url, data=payload)
         
+        if response.status_code == 200:
+            post_id = response.json().get('id')
+            print(f"Successfully posted: Post ID: {post_id}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+def getFacebook_page_id():
+    ''' 
+    Check if facebook_page_id.txt exists, if not have user enter page ID 
+    and create file, then return text from file
+    '''
+    try:
+        with open('facebook_page_id.txt', 'r') as f:
+            contents=f.read()
+            print("Page ID: " + contents)
+            return contents
+    except FileNotFoundError:
+        print("To get page ID, log in to facebook and ")
+        print("click about->page transparency->scroll to bottom for ID number")
+        contents=input("Enter Page ID: ")
+        with open('facebook_page_id.txt', 'w') as f:
+            f.write(contents)
+            f.close()
+        return contents
+        
+    
 if __name__ == "__main__":
     import sys
     '''
