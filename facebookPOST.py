98dccd854ee9ca23ca8a0e8af58cc9374c14059c
Joshua S. Andrews <91918985+Heero1224@users.noreply.github.com>
2026-04-09 17:51:58 -0700

    Update facebookPOST.py
    Added facebook_page_id as argument to get info from GUI

--- facebookPOST.py
+++ facebookPOST.py
@@ -4,10 +4,10 @@ author: Joshua S. Andrews
 purpose: to post a message to a facebook feed utilizing facebook-sdk
 """
 
-def img(token, message, image_path):
+def img(token, message, image_path,facebook_page_id):
     import requests
     
-    page_id=getFacebook_page_id()
+    page_id=getFacebook_page_id(facebook_page_id)
     
     post_url= f'https://graph.facebook.com/{page_id}/feed'
     
@@ -31,10 +31,10 @@ def img(token, message, image_path):
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
-def txt(token, msg):
+def txt(token, msg,facebook_page_id):
     import requests
     
-    page_id=getFacebook_page_id()
+    page_id=getFacebook_page_id(facebook_page_id)
     
     post_url= f'https://graph.facebook.com/{page_id}/feed'
     
@@ -51,25 +51,30 @@ def txt(token, msg):
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
-def getFacebook_page_id():
+def getFacebook_page_id(facebook_page_id):
     ''' 
     Check if facebook_page_id.txt exists, if not have user enter page ID 
     and create file, then return text from file
     '''
-    try:
-        with open('facebook_page_id.txt', 'r') as f:
-            contents=f.read()
-            print("Page ID: " + contents)
+    if (facebook_page_id is ""):
+        try:
+            with open('facebook_page_id.txt', 'r') as f:
+                contents=f.read()
+                print("Page ID: " + contents)
+                return contents
+        except FileNotFoundError:
+            print("To get page ID, log in to facebook and ")
+            print("click about->page transparency->scroll to bottom for ID number")
+            contents=input("Enter Page ID: ")
+            with open('facebook_page_id.txt', 'w') as f:
+                f.write(contents)
+                f.close()
             return contents
-    except FileNotFoundError:
-        print("To get page ID, log in to facebook and ")
-        print("click about->page transparency->scroll to bottom for ID number")
-        contents=input("Enter Page ID: ")
+    else:
         with open('facebook_page_id.txt', 'w') as f:
-            f.write(contents)
-            f.close()
-        return contents
-        
+            f.write(facebook_page_id)
+            f.close
+        return facebook_page_id        
     
 if __name__ == "__main__":
     import sys
@@ -78,11 +83,12 @@ if __name__ == "__main__":
         1 = access_token
         2 = message
         3 = image_path
+        4 = facebook_page_id
     '''
     length=len(sys.argv)
-    if length==2:
-        txt(sys.argv[1],sys.argv[2])
-    elif length==3:
-        img(sys.argv[1],sys.argv[2],sys.argv[3])
+    if length==3:
+        txt(sys.argv[1],sys.argv[2], sys.argv[3])
+    elif length==4:
+        img(sys.argv[1],sys.argv[2],sys.argv[3], sys.argv[4])
     else:
-        print("Needs [Token, Message, <image>] to POST to facebook")
+        print("Needs [Token, Message, <image_path>, facebook_page_id] to POST to facebook")
