4949fc052b79ac8c4a35852d4281d208ceb8b4d7
Joshua S. Andrews <91918985+Heero1224@users.noreply.github.com>
2026-04-13 13:14:55 -0700

    Update main.py
    added returns to function

--- main.py
+++ main.py
@@ -50,9 +50,9 @@ eel.init("web")
 @eel.expose
 def postToFacebook(text,image, facebook_page_id):
     if (image==""):
-        facebookPOST.txt(user_access_token,text, facebook_page_id)
+        return facebookPOST.txt(user_access_token,text, facebook_page_id)
     else:
-        facebookPOST.img(user_access_token,text,image, facebook_page_id)
+        return facebookPOST.img(user_access_token,text,image, facebook_page_id)
 
 @eel.expose
 def postToInstagram(text):
