e383f3038b22548696bfcc4c2ce2331debcd1f08
Joshua S. Andrews <91918985+Heero1224@users.noreply.github.com>
2026-04-24 19:14:07 -0700

    Update facebookPOST.py
    Fixed to post to facebook business page

--- facebookPOST.py
+++ facebookPOST.py
@@ -4,54 +4,94 @@ author: Joshua S. Andrews
 purpose: to post a message to a facebook feed utilizing facebook-sdk
 """
 
-def img(token, msg, image_path,facebook_page_id):
+def img(access_token, msg, image_path, page_id):
     print("Posting Image")
+    import facebook
+    graph = facebook.GraphAPI(access_token=access_token)
+    
+    with open(image_path, 'rb') as image:
+        graph.put_photo(parent_object=page_id, connection_name='feed',image=image, message=msg)
+
+def txt(access_token, msg, page_id):
+    import facebook
+    graph = facebook.GraphAPI(access_token=access_token)
+    graph.put_object(parent_object=page_id, connection_name='feed', message=msg)
+
+def get_token():
+    """Gets a long-lived page access token from graph api"""
     import requests
-    
-    page_id=getFacebook_page_id(facebook_page_id)
-    
-    post_url= f'https://graph.facebook.com/{page_id}/feed'
-    
+    pages = get_page_access_token()
+    page_access_token = pages['data'][0]['access_token']
+    app_id = '1760179784929581'
+    app_secret = '0e3a54441382b1b40fd1ccb6c08197de'
+    graph_ver = "v25.0"
+
+    url = f"https://graph.facebook.com/{graph_ver}/oauth/access_token"
     params = {
-        'access_token': token
+        'grant_type': 'fb_exchange_token',
+        'client_id': app_id,
+        'client_secret': app_secret,
+        'fb_exchange_token': page_access_token
     }
-    data = {
-        'message': msg
-    }
-    files = {
-        'source': open(image_path, 'rb')
-    }
-    
+    response = requests.get(url, params=params)
+    return response.json().get('access_token')
 
-    try:
-        response = requests.post(url=post_url, params=params, data=data, files=files)
-        
-        if response.status_code == 200:
-            post_id = response.json().get('id')
-            return (f"Successfully posted: Post ID: {post_id}")
-    except requests.exceptions.RequestException as e:
-        return (f"An error occurred: {e}")
-
-def txt(token, msg,facebook_page_id):
-    print("Posting Text")
+def get_page_access_token():
+    """Gets a short-lived page access token from graph API for
+        all managed pages
+    """
     import requests
-    
-    page_id=getFacebook_page_id(facebook_page_id)
-    
-    post_url= f'https://graph.facebook.com/{page_id}/feed'
-    
-    payload = { 'message': msg,
-                'access_token': token
+    user_token = get_user_token()
+    graph_ver = 'v25.0'
+
+    url = f"https://graph.facebook.com/{graph_ver}/me/accounts"
+    params = {
+        'access_token': user_token
     }
-    
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if 'data' in data:
+        for page in data['data']:
+            print(f"Page Name: {page['name']}")
+            print(f"Page ID: {page['id']}")
+            print(f"Page Access Token: {page['access_token']}")
+            print("-"*30)
+    else:
+        print("Error or no pages found:", data)
+    return data
+
+def get_user_token():
+    """Gets a long-lived user token for app from graphAPI"""
     try:
-        response = requests.post(post_url, data=payload)
-        
-        if response.status_code == 200:
-            post_id = response.json().get('id')
-            return (f"Successfully posted: Post ID: {post_id}")
-    except requests.exceptions.RequestException as e:
-        return (f"An error occurred: {e}")
+        with open('token.txt', 'r') as f:
+            print("token.txt exists!")
+    except FileNotFoundError:
+        import facebook
+        token=get_short_user_token()
+        graph = facebook.GraphAPI(access_token=token)
+        app_id = '1760179784929581'
+        app_secret = '0e3a54441382b1b40fd1ccb6c08197de'
+        long_token= graph.extend_access_token(app_id=app_id, app_secret=app_secret)
+        print(long_token)
+        with open('token.txt', 'w') as f:
+            f.write(str(long_token['access_token']))
+            f.close()
+        return long_token
+    except IOError:
+        print("File [token.txt] exists but is not accessible.")
+    with open('token.txt', 'r') as f:
+        contents = f.read()
+        f.close()
+        return contents
+
+def get_short_user_token():
+    """
+    Returns manually entered short-lived token from browser based
+    Graph API Explorer as a string
+    """
+    return "EAAZAA38rSUS0BRWG4tncTzOzJEOeUDwExU15G2XQUZCLjBDL7v9XZAlp0fIQ7maFZCXSZA9slO5giuWyfc0Y0XWAhZAZCugE34xJBMaTb4vwW5OeXEItqQO4ZBURZAtmvUHN4o5N8q2SNUqKB1vDG1ZCFZA2UjdbiARzcfzBQchqAcdP4fpXKqDzGlXzkWvZAjZA5qzp5M1ZA8ZAuFCtdSChVWygEApw3khPUAL5VbotkR2xI3ZB358uJUSRA0yKHMJ21fEGjpZCPa8BZBhP8bJZA32JuHaZB7RZCDgZDZD"
+
 
 def getFacebook_page_id(facebook_page_id):
     '''
@@ -75,7 +115,7 @@ def getFacebook_page_id(facebook_page_id):
     else:
         with open('facebook_page_id.txt', 'w') as f:
             f.write(facebook_page_id)
-            f.close
+            f.close()
         return facebook_page_id
     
 if __name__ == "__main__":
